901fc7d8131e4dd04f052745eadb8ef56ed726cb
Mike Cecconello <mikececco2000@gmail.com>
2023-04-26 18:02:31 +0200

    Added user unit tests and first workflow

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
+# test/models/user_test.rb
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "full_name returns the capitalized first name and last name" do
+    user = User.new(first_name: "john", last_name: "lennon")
+    assert_equal "John Lennon", user.full_name
+  end
+
+  test "first_name returns first name" do #test model behaviour
+    user = User.new(first_name: "Andy")
+    assert_equal "Andy", user.first_name
+  end
 end
